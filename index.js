5f44ba1128b24e08012f0ba7172e0269080ff7cd
Brook Yang <uedsky@gmail.com>
2016-07-28 22:01:27 +0800

    add qr

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var app = express();
 app.set('view engine', 'pug');
@@ -7,6 +9,22 @@ app.locals.env = {
 
 app.get('/', function (req, res) {
   res.render('index');
+});
+
+app.get('/qr', function(req, res) {
+  var qr = require('qr-image');
+  var imageType = req.query.type || 'svg';
+  res.type(imageType);
+  let text = req.query.text || req.get('Referrer');
+  if(!text) {
+    res.send(
+`<svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" viewBox="0 0 300 300">
+  <text x="0" y="0" >Missing parameter text</text>
+</svg>`
+    ); res.end();
+  }
+  var qrImage = qr.image(text, { type: imageType, size: 100 });
+  qrImage.pipe(res);
 });
 
 app.listen(3333, function () {
