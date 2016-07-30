cf9b9e7e68a1c5c52ddffec4dc51cc2c4b9df4f7
Brook Yang <uedsky@gmail.com>
2016-07-30 11:23:42 +0800

    add desktop-only qr

--- index.js
+++ index.js
@@ -4,13 +4,44 @@ var express = require('express');
 var app = express();
 app.set('view engine', 'pug');
 app.set('debug', app.get('env') !== 'production');
+app.disable('x-powered-by');
+
+app.use(function(req, res, next) {
+  if(app.get('debug')) {
+    console.error('[%s] %s %s', new Date().toLocaleString(), req.method, req.originalUrl);
+  }
+  next();
+});
 
 app.get('/', function (req, res) {
   res.render('index');
 });
 
 app.get('/qr', function(req, res) {
-  var qr = require('qr-image');
+  let text = req.query.text;
+
+  // 同域的请求 text 为空时，使用 referrer
+  let errorMsg = '', referrer = req.get('Referrer');
+  let topDomain = req.hostname.replace(/.*?(?=\w+\.\w+$)/g, '');
+  if(!text && referrer && new RegExp('https?://(\\w+\\.)*(' + topDomain + '|brook.dev)(?=/|:)').test(referrer)) {
+    var detector = (function(ua) {
+      return {
+        mobile: function() {
+          return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/.test(ua)
+        }
+      }
+    })(req.headers['user-agent']);
+    if(req.query['desktop-only'] && detector.mobile()) {
+      res.type('gif');
+      res.end(new Buffer('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7', 'base64'));
+    } else {
+      const querystring = require('querystring');
+      req.query.text = referrer;
+      res.redirect('?' + querystring.stringify(req.query));
+    }
+    return;
+  }
+
   var options = {
     size: 200,
     type: 'svg',
@@ -21,25 +52,18 @@ app.get('/qr', function(req, res) {
       options[option] = req.query[option];
     }
   }
-
   res.type(options.type);
-  let text = req.query.text;
-
-  // 同域的请求 text 为空时，使用 referrer
-  let errorMsg = '', referrer = req.get('Referrer');
-  let topDomain = req.hostname.replace(/.*?(?=\w+\.\w+$)/g, '');
-  if(!text && referrer && new RegExp('https?://(\\w+\\.)*' + topDomain + '\\b').test(referrer)) {
-    text = referrer;
-  }
   if(!text) {
-    res.send(
+    res.end(
 `<svg xmlns="http://www.w3.org/2000/svg" width="${options.size}" height="${options.size}" viewBox="0 0 ${options.size} ${options.size}">
   <text x="50%" y="50%" text-anchor="middle">${errorMsg || 'Missing parameter text'}</text>
 </svg>`
     );
-    res.end();
+    return;
   }
 
+  var qr = require('qr-image');
+  res.setHeader('Cache-Control', 'public, max-age=31557600');
   options.size = options.size / 21;
   options.margin = options.margin / 10;
   var qrImage = qr.image(text, options);
